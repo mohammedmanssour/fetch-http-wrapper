9d71f2833c64cce6df41030b72e6b50c87a4ccee
Mohammed Manssour <manssour.mohammed@gmail.com>
2018-12-26 10:57:08 +0200

    add a function to convert an object to query string

---
+++ src/ToQueryString.ts
@@ -0,0 +1,25 @@
+export interface QueryObject {
+  [key: string]: string | string[];
+}
+
+export function toQueryString(object: QueryObject): string {
+  let query: string[] = [];
+
+  for (var key in object) {
+    if (Array.isArray(object[key])) {
+      query.push(arrayToQueryString(key, object[key] as string[]));
+    } else {
+      query.push(`${key}=${object[key]}`);
+    }
+  }
+
+  return query.join('&');
+}
+
+export function arrayToQueryString(key: string, array: string[]): string {
+  let query: string[] = [];
+  for (let index = 0; index < array.length; index++) {
+    query.push(`${key}[${index}]=${array[index]}`);
+  }
+  return query.join('&');
+}
---
+++ tests/test-ToQueryString.js
@@ -0,0 +1,23 @@
+import test from 'ava';
+import { toQueryString } from '../dist/ToQueryString';
+
+test('can convert object to query string', t => {
+  const query = {
+    key: 'value',
+    key2: 'value2'
+  };
+
+  t.deepEqual('key=value&key2=value2', toQueryString(query));
+});
+
+test('can convert array to query string', t => {
+  const query = {
+    key: ['value', 'value2', 'value3'],
+    key2: 'value2'
+  };
+
+  t.deepEqual(
+    'key[0]=value&key[1]=value2&key[2]=value3&key2=value2',
+    toQueryString(query)
+  );
+});
