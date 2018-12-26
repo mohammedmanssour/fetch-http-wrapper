36ba086dc2e247d384f652bce12d31334d1e4118
Mohammed Manssour <manssour.mohammed@gmail.com>
2018-12-26 10:00:37 +0200

    FetchRequest class

---
+++ src/lib/FetchRequest.ts
@@ -0,0 +1,131 @@
+import FetchOptions from './FetchOptions';
+
+export default class FetchRequest {
+  options: FetchOptions = new FetchOptions();
+
+  /**
+   * set reqyest url
+   * @param url
+   * @returns FetchRequest
+   */
+  setUrl(url: string): FetchRequest {
+    this.options.url = url;
+    return this;
+  }
+
+  /**
+   * set request method
+   * @param method
+   * @returns FetchRequest
+   */
+  withMethod(method: string): FetchRequest {
+    this.options.method = method;
+    return this;
+  }
+
+  /**
+   * set request params
+   * @param method
+   * @returns FetchRequest
+   */
+  withParams(params: any): FetchRequest {
+    this.options.body = params;
+    return this;
+  }
+
+  /**
+   * add a param to the params list
+   * @param key
+   * @param value
+   * @returns FetchRequest
+   */
+  param(key: string, value: any): FetchRequest {
+    this.options.body[key] = value;
+    return this;
+  }
+
+  /**
+   * set headers
+   * @param headers
+   * @returns FetchRequest
+   */
+  withHeaders(headers: { [key: string]: string }): FetchRequest {
+    this.options.headers = headers;
+    return this;
+  }
+
+  /**
+   * add a header to the headers list
+   * @param key
+   * @param value
+   * @returns FetchRequest
+   */
+  header(key: string, value: any): FetchRequest {
+    this.options.headers[key] = value;
+    return this;
+  }
+
+  /**
+   * set request credentials options
+   * https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#Sending_a_request_with_credentials_included
+   * @param credentials
+   * @returns FetchRequest
+   */
+  credentials(credentials: string): FetchRequest {
+    this.options.credentials = credentials;
+    return this;
+  }
+
+  /**
+   * set request model
+   * https://developer.mozilla.org/en-US/docs/Web/API/Request/mode
+   * @param mode
+   * @returns FetchRequest
+   */
+  mode(mode: string): FetchRequest {
+    this.options.mode = mode;
+    return this;
+  }
+
+  /**
+   * set request cache mode
+   * https://developer.mozilla.org/en-US/docs/Web/API/Request/cache
+   * @param cache
+   * @returns FetchRequest
+   */
+  cache(cache: string): FetchRequest {
+    this.options.cache = cache;
+    return this;
+  }
+
+  /**
+   * set request redirect mode
+   * https://developer.mozilla.org/en-US/docs/Web/API/Request/Request
+   * @param redirect
+   */
+  redirect(redirect: string): FetchRequest {
+    this.options.redirect = redirect;
+    return this;
+  }
+
+  /**
+   * set request referrer
+   * https://developer.mozilla.org/en-US/docs/Web/API/Request/Request
+   * @param referrer
+   * @return FetchRequest
+   */
+  referrer(referrer: string): FetchRequest {
+    this.options.referrer = referrer;
+    return this;
+  }
+
+  /**
+   * set request integrity
+   * @param integrity
+   * @returns FetchRequest
+   */
+  integrity(integrity: string): FetchRequest {
+    this.options.integrity = integrity;
+    return this;
+  }
+}
---
+++ tests/test-FetchRequest.js
@@ -0,0 +1,101 @@
+import test from 'ava';
+import FetchRequest from '../dist/lib/FetchRequest';
+
+function initFetchRequest() {
+  let request = new FetchRequest();
+
+  return request;
+}
+
+test('request can set options url', t => {
+  let request = initFetchRequest();
+  request.setUrl('http://mohammedmanssour.me');
+  t.deepEqual('http://mohammedmanssour.me', request.options.url);
+});
+
+test('request can set options method', t => {
+  let request = initFetchRequest();
+  t.deepEqual('get', request.withMethod('get').options.method);
+});
+
+test('request can set options params', t => {
+  let request = initFetchRequest();
+  t.deepEqual(
+    {
+      key: 'value'
+    },
+    request.withParams({
+      key: 'value'
+    }).options.body
+  );
+
+  t.deepEqual(
+    {
+      key: 'value',
+      key2: 'value2'
+    },
+    request.param('key2', 'value2').options.body
+  );
+});
+
+test('request can set options headers', t => {
+  let request = initFetchRequest();
+  t.deepEqual(
+    {
+      'Content-Type': 'application/json'
+    },
+    request.withHeaders({
+      'Content-Type': 'application/json'
+    }).options.headers
+  );
+
+  t.deepEqual(
+    {
+      'Content-Type': 'application/json',
+      accepts: 'application/json'
+    },
+    request.header('accepts', 'application/json').options.headers
+  );
+});
+
+test('request can set options credentials', t => {
+  let request = initFetchRequest();
+  t.deepEqual(
+    'some-credentials',
+    request.credentials('some-credentials').options.credentials
+  );
+});
+
+test('request can set options mode', t => {
+  let request = initFetchRequest();
+  t.deepEqual('some-mode', request.mode('some-mode').options.mode);
+});
+
+test('request can set options cache', t => {
+  let request = initFetchRequest();
+  t.deepEqual('some-cache', request.cache('some-cache').options.cache);
+});
+
+test('request can set options redirect', t => {
+  let request = initFetchRequest();
+  t.deepEqual(
+    'some-redirect',
+    request.redirect('some-redirect').options.redirect
+  );
+});
+
+test('request can set options referrer', t => {
+  let request = initFetchRequest();
+  t.deepEqual(
+    'some-referrer',
+    request.referrer('some-referrer').options.referrer
+  );
+});
+
+test('request can set options integrity', t => {
+  let request = initFetchRequest();
+  t.deepEqual(
+    'some-integrity',
+    request.integrity('some-integrity').options.integrity
+  );
+});
