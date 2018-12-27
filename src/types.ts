2827949b92e087953be6ce8a6a9be8c729e23369
Mohammed Manssour <manssour.mohammed@gmail.com>
2018-12-27 15:46:10 +0200

    move Middleware types

---
+++ src/types.ts
@@ -0,0 +1,8 @@
+import FetchOptions from './lib/FetchOptions';
+import FetchRequest from './lib/FetchRequest';
+
+export type BeforeMiddleware = (
+  options: Partial<FetchOptions>
+) => Partial<FetchOptions>;
+
+export type AfterMiddleware = (request: FetchRequest, response: any) => any;
